b5be337ff5dc1372dd19ff60a984fe4d9237ccad
Victor Bolivar <victor.bolivar@pucp.edu.pe>
2022-11-27 10:58:03 -0500

    Cliente [+] Mientras la solicitud no sea 'Instalada', no se mostrara nada en los fragments 'Mi camara' y 'Capturas de video'

--- app/src/main/java/com/pucp/camerasecure/cliente/cliente_capturasvideo.java
+++ app/src/main/java/com/pucp/camerasecure/cliente/cliente_capturasvideo.java
@@ -1,19 +1,84 @@
 package com.pucp.camerasecure.cliente;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.pucp.camerasecure.R;
+
+import java.util.HashMap;
 
 
 public class cliente_capturasvideo extends Fragment {
 
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+
     public cliente_capturasvideo() {
-super(R.layout.fragment_cliente_capturasvideo);    }
+        super(R.layout.fragment_cliente_capturasvideo);
+        // initialize firebase auth
+        mAuth = FirebaseAuth.getInstance();
+        // initialize firebase database
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+
+        cargarDatos();
+
+        return view;
+    }
+
+    public void cargarDatos(){
+
+        mDatabase.child("users").child(mAuth.getCurrentUser().getUid()).get().addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) {
+                Log.e("msg / firebase", "Error getting data", task.getException());
+            }
+            else {
+                Log.d("msg / firebase", String.valueOf(task.getResult().getValue()));
+                HashMap<String, String> data = (HashMap<String, String>) task.getResult().getValue();
+
+                String estadoSolicitud = data.get("estadoSolicitud");
+                if (!estadoSolicitud.equals("Instalado")){
+                    // mensaje indicando al usuario que espere a que su solicitud sea aprobada
+                    AlertDialog.Builder builder1 = new AlertDialog.Builder(getContext());
+                    builder1.setMessage("Tu cámara aún no ha sido instalada. Por favor, espera a la confirmación del administrador para poder acceder a esta funcionalidad.");
+                    builder1.setCancelable(true);
+
+                    builder1.setPositiveButton(
+                            "Aceptar",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+
+                } else {
+                    // TODO cargar los datos
+                }
+
+
+            }
+        });
+
+    }
 
 }
